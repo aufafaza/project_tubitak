f2b0c0d794317776a31e0cfb94531d41da3c2188
aufafaza <aufa.rienaldifaza.a@gmail.com>
2026-06-05 20:03:52 +0700

    feat: arming done

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     try: 
         myDrone = Drone("udpin:localhost:14550")
 
-        while True: 
-            preArmMsg = myDrone.preArm()
-            if (preArmMsg != mu.mavlink.MAV_RESULT_ACCEPTED): 
-                print(f"prearm failed, do not arm {preArmMsg}") 
-            else: 
-                print(f"prearm successful with message {preArmMsg} (should be 0)")  
+        preArmMsg = myDrone.preArm()
+        if (preArmMsg != mu.mavlink.MAV_RESULT_ACCEPTED): 
+            print(f"prearm failed, do not arm {preArmMsg}") 
+        else: 
+            print(f"prearm successful with message {preArmMsg} (should be 0)")  
+            myDrone.setArm()
 
 
             
